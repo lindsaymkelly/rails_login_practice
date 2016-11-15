beb2c5ab7501f8105c84988d886014fa196481bc
lindsaymkelly <lindzlady@gmail.com>
2016-11-14 21:11:09 -0500

    Add logout functionality and incentive to do so

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user 
+      flash[:success] = "Welcome to this super cool app! It's super cool."
       redirect_to @user
     else
       render 'new'
